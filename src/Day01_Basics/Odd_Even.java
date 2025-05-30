f9f19c9e12f51f13a0a8ce88c34b846130e3d439
Sangeetha832 <jothisangee15@gmail.com>
2025-05-30 21:07:50 +0530

    Added Day 2 Java practice files – Arrays, Strings, Switch

---
+++ src/Day01_Basics/Odd_Even.java
@@ -0,0 +1,23 @@
+package Day01_Basics;
+
+import java.util.Scanner;
+
+//Check Even or Odd using if-else
+public class Odd_Even {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int number = scan.nextInt();
+		
+		Odd_Even obj = new Odd_Even();
+		obj.check_odd_even(number);
+		
+	}
+	public void check_odd_even(int number) {
+		if(number % 2 == 0) {
+			System.out.println("The number is Even");
+		}else {
+			System.out.println("The number is Odd ");
+		}
+	}
+}
+
